bd60d3a55bdb5822692904e31b6f8d19fc64f6d4
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-01-03 22:15:07 +0300

    Add quick links component

---
+++ src/components/QuickLinks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="grid-x">
+        <div class="medium-12 ">
+            <div class="pull-right">
+                <router-link v-for="component in components" :to="{ name: component.name }" class="button small">
+                    {{ component.caption }}
+                </router-link>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['components']
+    }
+</script>
